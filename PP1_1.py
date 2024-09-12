a18628bb965aecfdf5ad0ecd7f9880d9c9816624
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-12 17:27:30 +0000

    first commit

--- PP1_1.py
+++ PP1_1.py
@@ -2,15 +2,19 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
+  print(1)
 
 def q2():
   #Write Assignment code here
+  print(1)
   
 def q3():
   #Write Assignment code here
+  print(1)
   
 def q4():
   #Write Assignment code here
+  print(1)
 
 #Do not alter anything after this line
 q1()
