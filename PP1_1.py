ce91f33d220ea8152c061641f59baefafba6f4b1
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-12 13:56:07 -0400

    Update PP1_1.py

--- PP1_1.py
+++ PP1_1.py
@@ -2,20 +2,23 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-  print(1)
+  print("Hello World")
 
 def q2():
   #Write Assignment code here
   print(1)
   print(2)
+  print(3)
+  print(4)
+  print(5)
   
 def q3():
   #Write Assignment code here
-  print(1)
+  print(" Help Me!)
   
 def q4():
   #Write Assignment code here
-  print(1)
+  print("3 x 3 = 9")
 
 #Do not alter anything after this line
 q1()
